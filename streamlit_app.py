0687c247ff4b8be671e413e947eec2b1c9cb716e
Unmesh Manore <117805929+snowflakeacct2022@users.noreply.github.com>
2022-11-09 13:23:13 +0530

    Update streamlit_app.py
    Added fruit picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,9 @@ streamlit.text('🥑🍞Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 #Reading csv file from S3 bucket into data frame
 fruit_data_frame=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#Adding pick up list to customize smoothie
+streamlit.multiselect('Pick up some fruits:', list(fruit_data_frame.index))
+
 #Display dataframe
 streamlit.dataframe(fruit_data_frame)
