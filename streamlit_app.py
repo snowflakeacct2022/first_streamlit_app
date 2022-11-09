e2a37f74492e7b8ee9f9ad866e01f0c2503f3062
Unmesh Manore <117805929+snowflakeacct2022@users.noreply.github.com>
2022-11-09 14:43:49 +0530

    Update streamlit_app.py
    Added default selection to fruit multiselect picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ fruit_data_frame=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.c
 fruit_data_frame=fruit_data_frame.set_index('Fruit');
 
 #Adding pick up list to customize smoothie
-streamlit.multiselect('Pick up some fruits:', list(fruit_data_frame.index))
+streamlit.multiselect('Pick up some fruits:', list(fruit_data_frame.index),['Avocado','Strawberries'])
 
 #Display dataframe
 streamlit.dataframe(fruit_data_frame)
