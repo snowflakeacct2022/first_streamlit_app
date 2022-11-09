38b5fa7541948c5db281bd6826d2932f01ee3b91
Unmesh Manore <117805929+snowflakeacct2022@users.noreply.github.com>
2022-11-09 13:19:14 +0530

    Update streamlit_app.py
    Read csv file from S3 bucket in data frame and displayed the dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
@@ -7,3 +8,7 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+#Reading csv file from S3 bucket into data frame
+fruit_data_frame=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#Display dataframe
+streamlit.dataframe(fruit_data_frame)
